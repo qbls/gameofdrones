ef8224f946fcaff004092ee114a2c68d711cc34d
qbls <qqtin81@gmail.com>
2024-04-11 08:59:19 +0200

    MAJ DB

---
+++ req.php
@@ -0,0 +1,34 @@
+<?php
+// Paramètres de connexion à la base de données
+$servername = "10.5.40.44"; //
+$username = "root";
+$password = "root";
+$dbname = "gameofdrone";
+
+// Créer la connexion
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérifier la connexion
+if ($conn->connect_error) {
+    die("La connexion a échoué : " . $conn->connect_error);
+}
+
+// Récupérer les données de la requête POST
+$drone_id = $_POST['drone_id'];
+$latitude = $_POST['latitude'];
+$longitude = $_POST['longitude'];
+$altitude = $_POST['altitude'];
+$datetime = $_POST['datetime'];
+
+// Préparer et lier
+$stmt = $conn->prepare("INSERT INTO drone_positions (drone_id, latitude, longitude, altitude, datetime) VALUES (?, ?, ?, ?, ?)");
+$stmt->bind_param("sddds", $drone_id, $latitude, $longitude, $altitude, $datetime);
+
+// Exécuter la requête
+$stmt->execute();
+
+echo "Position enregistrée avec succès";
+
+$stmt->close();
+$conn->close();
+?>
